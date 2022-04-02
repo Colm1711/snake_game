97cc8baafcd622b7300220741bfe60db1d86adf0
Colm1711 <cosullivan1711@gmail.com>
2022-04-02 20:00:41 +0000

    add snake object

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,32 @@ let speed = 5;
 
 //Add code to create grid inside the canvas
 let tileCount = 20;
-let tileSize = canvas.width/tileCount -1;//this will make the tile size fit inside the grid
+let tileSize = canvas.width / tileCount -2;//this will make the tile size fit inside the grid
 
-//game loop
+//create the snake object
+let headX = 7;
+let headY = 3;
 
+
+//game loop function, controls the overall game
 function snakeGame(){
+    //creates game screen
     gameScreen();
+    //adds snake to canvas
+    gameSnake();
     setTimeout(snakeGame, 1000/speed);
 }
 
 //need to create the screen to run game and to reset when game starts again
-
 function gameScreen(){
     ctx.fillStyle = "black";
     ctx.fillRect(0 ,0 , canvas.width, canvas.height);
+}
+
+//snake function to draw the snake to cnavas
+function gameSnake(){
+    ctx.fillStyle = "green"; //setting snake to green, need to style later for head/body
+    ctx.fillRect(headX* tileCount, headY* tileCount, tileSize, tileSize);
 }
 
 snakeGame();
