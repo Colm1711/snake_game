b3c3802139e6a3cb7c5e483c01b093d09b3366b4
Colm1711 <cosullivan1711@gmail.com>
2022-04-02 19:46:21 +0000

    add game function loop and add canvas styling and grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 //creating canvas objects
 const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.textContent('2d');
+const ctx = canvas.getContext('2d');
 
+//creating a default speed which I will use in game function
+let speed = 5;
 
+//Add code to create grid inside the canvas
+let tileCount = 20;
+let tileSize = canvas.width/tileCount -1;//this will make the tile size fit inside the grid
 
+//game loop
+
+function snakeGame(){
+    gameScreen();
+    setTimeout(snakeGame, 1000/speed);
+}
+
+//need to create the screen to run game and to reset when game starts again
+
+function gameScreen(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0 ,0 , canvas.width, canvas.height);
+}
+
+snakeGame();
