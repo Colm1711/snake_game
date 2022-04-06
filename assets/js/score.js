d954cfd08b20276e3386703f8f256467c3d4438f
Colm1711 <cosullivan1711@gmail.com>
2022-04-06 15:13:46 +0000

    Adjusted gameScore poistioning

--- assets/js/score.js
+++ assets/js/score.js
@@ -4,6 +4,16 @@
 //importing score var from gameOver
 import {score} from './script.js';
 
+
+//collecting user name to set as Key 
+function myMsg(){
+    let player = prompt("Please enter your name:");
+    console.log(player);
+}
+
+
+  
+
 // add event listener to button where user can update there score to list on HTML doc
 let btn = document.getElementById('scoreButton')
     btn.addEventListener('click', function scoreBoard(){
@@ -14,4 +24,4 @@ let btn = document.getElementById('scoreButton')
         document.getElementById('scoreList').appendChild(listItem);
     });
     
-
+    myMsg();
