5a43aa7ffa496cd20e6933fabe9bf0b5ef7effc5
Colm1711 <cosullivan1711@gmail.com>
2022-04-06 10:34:43 +0000

    Add score file to update score board for user at end game

---
+++ assets/js/score.js
@@ -0,0 +1,17 @@
+//This contains the code for the score board that can either be displayed by clicking button to reveal//
+//Or will display on end of game//
+
+//importing score var from gameOver
+import {score} from './script.js';
+
+// add event listener to button where user can update there score to list on HTML doc
+let btn = document.getElementById('scoreButton')
+    btn.addEventListener('click', function scoreBoard(){
+        let listItem = document.createElement('li');
+        let newScore = document.createTextNode(score);
+        
+        listItem.appendChild(newScore);
+        document.getElementById('scoreList').appendChild(listItem);
+    });
+    
+
