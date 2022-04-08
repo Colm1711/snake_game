00d94bbd0166041cf18416c43918dda836382719
Colm1711 <cosullivan1711@gmail.com>
2022-04-08 13:38:31 +0000

    Rename file, add check for session storage on user name

--- assets/js/username.js
+++ assets/js/username.js
@@ -3,9 +3,15 @@
 
 //collecting user name to set as Key 
 function setUserName(){
+    let checkName = sessionStorage.getItem('PlayerName');
+    //if statement to if username is set in session storage
+    if(checkName){
+        alert(`Welcome back ${checkName}!`)
+    }else{
     let player = prompt("Please enter your name:");
     //setting to session storage in case change of player
     sessionStorage.setItem('PlayerName', JSON.stringify(player));
+    }
 }
 
 setUserName();
