99ff3f01ab632e9c9081f3f0422d19168f7a820c
Colm1711 <cosullivan1711@gmail.com>
2022-04-11 19:35:57 +0000

    darkmode handling and saving to local storage

---
+++ assets/js/darkmode.js
@@ -0,0 +1,23 @@
+// Select the toggle button
+const btn = document.querySelector(".btn-toggle-drk");
+// get theme from localStorage
+const currentTheme = localStorage.getItem("Theme");
+// Check if theme is darkmode in storage
+if (currentTheme == "dark") {
+  //add .dark-theme class to body
+  document.body.classList.add("dark-mode");
+}
+// Listen for toggle button to be used 
+btn.addEventListener("click", function () {
+//Toggle the .dark-mode class on each click
+  document.body.classList.toggle("dark-mode");
+//defult set theme to light
+  let theme = "light";
+//checks if dark-mode class ha been added
+  if (document.body.classList.contains("dark-mode")) {
+      //sets them to dark
+    theme = "dark";
+  }
+  //saves to localstorage
+  localStorage.setItem("Theme", theme);
+});
