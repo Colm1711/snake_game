5ff29e934fabc1cbfe3b783d77433a381cc1951f
Colm1711 <cosullivan1711@gmail.com>
2022-04-13 15:50:21 +0000

    add mute and umnute to button

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -12,3 +12,20 @@ openBtn.addEventListener('click', (e) =>{
 closBtn.addEventListener('click', (e) =>{
     modalObj.style.display='none';
 });
+
+
+let mute = document.getElementById('mute-button');
+let audio = document.querySelector('audio')
+mute.addEventListener('click', run);
+
+function run(){
+    if (mute.innerclass === 'fa-solid fa-volume-xmark'){
+        mute.innerclass = "fa-solid fa-volume"
+        audio.play();
+    }
+    else{
+        mute.innerclass = "fa-solid fa-volume-xmark"
+        audio.pause();
+    }
+        
+}
